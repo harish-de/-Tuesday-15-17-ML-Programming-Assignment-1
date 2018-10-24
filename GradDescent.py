29aee53444c8e6a8c1e664779837fb252c5849f7
Abhimanyu <40955141+785pavan@users.noreply.github.com>
2018-10-25 00:38:23 +0200

    added data to repository

--- GradDescent.py
+++ GradDescent.py
@@ -1,15 +1,15 @@
 import numpy as np
 import sys
 
+if __name__ == '__main__':
+    def get_data():
+        my_data = np.genfromtxt(sys.argv[1], delimiter=',')
+        X = my_data[:, 0:-1]
+        ones = np.ones([X.shape[0], 1])
+        X = np.concatenate((ones, X), 1)
+        y = my_data[:, -1].reshape(-1, 1)
+        return X, y
 
-def get_data():
-    my_data = np.genfromtxt(sys.argv[1], delimiter=',')
-    X = my_data[:, 0:-1]
-    ones = np.ones([X.shape[0], 1])
-    X = np.concatenate((ones, X), 1)
-    y = my_data[:, -1].reshape(-1, 1)
-    return X, y
 
-
-X, y = get_data()
-print(X)
+    X, y = get_data()
+    print(X)
