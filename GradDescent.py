a199fd64ec3a29abe31ad5f9ed6a75c1bfb7c07e
Abhimanyu <40955141+785pavan@users.noreply.github.com>
2018-10-25 00:03:05 +0200

    Final form for get_data( ) function
    done with get data function

--- GradDescent.py
+++ GradDescent.py
@@ -1,12 +1,18 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import sys
 import csv
-def get_next():
+
+
+def get_data():
     my_data = np.genfromtxt('data.csv', delimiter=',')
-    X = my_data[:, -2].reshape(-1, 1)
-    ones = np.ones([X.shape[0],1])
-    X = np.concatenate([ones, X],1)
-    y = my_data[:,-1].reshape(-1,1)
-    print(X)
-    print(y)
-get_next()
+    X = my_data[:, 0:-1]
+    ones = np.ones([X.shape[0], 1])
+    X = np.concatenate((ones, X), 1)
+    y = my_data[:, -1].reshape(-1, 1)
+    return X, y
+
+
+X, y = get_data()
+print(X[5][6])
+print(y[5])
