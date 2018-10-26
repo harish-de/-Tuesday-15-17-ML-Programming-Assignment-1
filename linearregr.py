de5134fc17422fab9f082b5f2ea52fd769ba3b72
Abhimanyu <40955141+785pavan@users.noreply.github.com>
2018-10-26 18:29:05 +0200

    resolved while loop

--- linearregr.py
+++ linearregr.py
@@ -5,8 +5,7 @@ import csv
 if __name__ == '__main__':
     # function to get data from console
     def get_data():
-        # my_data = np.genfromtxt(sys.argv[1], delimiter=',')
-        my_data = np.genfromtxt('yacht.csv', delimiter=',')
+        my_data = np.genfromtxt(sys.argv[1], delimiter=',')
         X = my_data[:, 0:-1]
         ones = np.ones([X.shape[0], 1])
         X = np.concatenate((ones, X), 1)
@@ -20,38 +19,35 @@ if __name__ == '__main__':
         return np.sum(summation) / (2 * len(X))
 
 
+    #error calculation
     def get_error(X,y,theta):
-        return np.sum(X @ theta.T - y)
+        return np.sum((X @ theta.T - y)**2)
 
 
     # gradient descent
     def gradient_descent(X, y, theta, alpha, threshold):
         iter = 0
-        cost = np.zeros(27400)
-        #print(get_error(X, y, theta))
-        writer = csv.writer(open("results.csv", 'w'), newline = "")
         while True:
             temp = theta
-            theta = theta - (alpha / len(X)) * (np.sum((X @ theta.T - y) * X, axis=0))
+            error = ((alpha ) * (np.sum((X @ theta.T - y) * X, axis=0)))
+            theta = theta - error
             cost = compute_cost(X, y, theta)
             iter += 1
-            output_string_a = str(iter)
-            output_string_b = str(theta[0])
-            output_string_c = str(np.sum((X @ theta.T - y)**2))
-            writer.writerow([output_string_a + " "])
-            writer.writerow([output_string_b.replace("[","").replace("]","") + ","])
-            writer.writerow([output_string_c])
-            if float(get_error(X, y, theta) - get_error(X, y, temp )) <= threshold:
+            next_theta = get_error(X, y, theta)
+            now_theta = get_error(X, y, temp)
+            # ittheta = np.concatenate(([iter], theta[0]), 0)
+            # np.savetxt("results.csv", np.concatenate((ittheta,[np.sum((X @ theta.T - y) * X)]),0))
+            print(iter, theta[0], now_theta)
+            if now_theta - next_theta <= threshold:
                 break
-        print(output_string_a, output_string_b.replace("[","").replace("]",""), output_string_c)
-        return iter, theta, cost
+        return iter, theta[0], now_theta
 
 
     X, y = get_data()
-    alpha = 0.0001  # float(sys.argv[2]) learning rate
-    threshold = 0.0001  # float(sys.argv[3])
+    alpha = float(sys.argv[2]) # learning rate
+    threshold = float(sys.argv[3]) # threshold
     theta = np.zeros([1, X.shape[1]])
-    iter, g, cost = gradient_descent(X, y, theta, alpha, threshold)
-    #print(g)
+    iter, g, error = gradient_descent(X, y, theta, alpha, threshold)
+    #print((iter), (theta[0]),error)
     final_cost = compute_cost(X, y, g)
     #print(final_cost)
